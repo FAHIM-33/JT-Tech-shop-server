b5c28fa7960efce51abe1bd4802cefa8434c54fa
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-02-21 20:52:30 +0600

    Mondodb setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.4pbmvpd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send("Server is up")
+})
+
+app.listen(port, () => { console.log("Server running at:", port); })
